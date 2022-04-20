d9fa5483fbf6e8511a077c419269f94d679bf2db
Bien <biendt@vnpay.vn>
2022-04-20 16:40:06 +0700

    update

--- src/main/java/com/autohome/be/service/impl/UsersServiceImpl.java
+++ src/main/java/com/autohome/be/service/impl/UsersServiceImpl.java
@@ -1,2 +1,18 @@
-package com.autohome.be.service.impl;public class UsersServiceImpl {
+package com.autohome.be.service.impl;
+
+import com.autohome.be.dto.response.Response;
+import com.autohome.be.repository.UsersRepository;
+import com.autohome.be.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersServiceImpl implements UsersService {
+    @Autowired
+    UsersRepository usersRepository;
+
+    @Override
+    public Response<String> login(String authenRequest) {
+        return null;
+    }
 }
